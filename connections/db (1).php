c26069572e769dc979ef26a1cf2a47e7776276c8
Boozmanras <74321508+Boozmanras@users.noreply.github.com>
2021-05-04 22:32:38 +0300

    Add files via upload

---
+++ connections/db (1).php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "sql102.epizy.com";
+$db_username = "epiz_28354383";
+$db_password = "ZflJogxxbEFlM";
+$database = "epiz_28354383_gloom";
+
+
+// initializing variables
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect($servername, $db_username, $db_password, $database);
+?>
